c99472d94de00b18cae67fef7d90e52d6a6b621a
brandoncallah <brandoncallah@gmail.com>
2025-10-31 09:15:11 -0400

    Fixed an issue where shorter prefixes were missed when a node was an end node and had children

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@
 
 from .pyTriEX import build_regex_from_list, Trie, TrieNode
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["build_regex_from_list", "Trie", "TrieNode"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="TrieFastRegex",
-    version="0.1.0",
+    version="0.1.1",
     author="Brandon Callahan",
     author_email="brandoncallah@gmail.com",
     description="A compact regular expression builder from string lists using Trie data structure",
